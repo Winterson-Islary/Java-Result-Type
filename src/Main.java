e364f8a7b37cf0086fb4d20ea730c913c068e56d
Winterson <u19cse1028@cit.ac.in>
2025-07-09 20:28:11 +0530

    modify: try out new method impelemntations

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("This is a test for Result type implementation in Java");
 		UserService userService = new UserService();
-		Result<String, UserError> result = userService.getUserById(4);
+		Result<String, UserError> result = userService.getUserById(1);
 
 		if (result.isOk())
-			System.out.println("User: " + result.unwrap());
+			System.out.println("User: " + result.map(value -> value.toUpperCase()).unwrap());
 		else {
 			UserError error = result.unwrapErr();
 			System.out.println("Error [" + error.getCode() + "]: " + error.getMessage());

f3e892017cdade76101fe6d30b19ca9ae4ff173b
Winterson <u19cse1028@cit.ac.in>
2025-07-09 19:29:01 +0530

    add: main entry point

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import models.errors.UserError;
+import services.user.UserService;
+import types.result.Result;
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("This is a test for Result type implementation in Java");
+		UserService userService = new UserService();
+		Result<String, UserError> result = userService.getUserById(4);
+
+		if (result.isOk())
+			System.out.println("User: " + result.unwrap());
+		else {
+			UserError error = result.unwrapErr();
+			System.out.println("Error [" + error.getCode() + "]: " + error.getMessage());
+		}
+	}
+}
